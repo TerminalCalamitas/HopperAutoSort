74ba8eaee3753e00e5d758dc3786d25af67b5f3f
caleb <paracordcaleb@gmail.com>
2023-11-24 09:31:18 -0500

    I guess I forgot to push after stealing LO's code

--- src/main/java/com/terminalcalamitas/hopperautosort/HopperAutoSort.java
+++ src/main/java/com/terminalcalamitas/hopperautosort/HopperAutoSort.java
@@ -1,13 +1,18 @@
 package com.terminalcalamitas.hopperautosort;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import com.terminalcalamitas.hopperautosort.listeners.HopSort;
 
 public final class HopperAutoSort extends JavaPlugin {
 
+    private static HopperAutoSort plugin;
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        plugin = this;
+        System.out.println("####Starting Hopper Sorting Plugin####");
+        getServer().getPluginManager().registerEvents(new HopSort(), this);
+       // getServer().getPluginManager().registerEvents(new onInventoryPickupItemEvent(this), this);
     }
 
     @Override
---
+++ src/main/java/com/terminalcalamitas/hopperautosort/listeners/HopSort.java
@@ -0,0 +1,69 @@
+package com.terminalcalamitas.hopperautosort.listeners;
+
+import org.bukkit.block.Container;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
+import org.bukkit.event.inventory.InventoryPickupItemEvent;
+import org.bukkit.event.inventory.InventoryType;
+
+import java.util.Arrays;
+
+public class HopSort implements Listener {
+
+    public HopSort() {}
+
+    private String getItemName (String translationKey) {
+        if(translationKey == null) return null;
+        String[] names = translationKey.split("\\.");
+        return names[names.length-1];
+    }
+
+    private boolean filterMatch (String filterString, String fullItemName) {
+        String itemName = getItemName(fullItemName);
+        String[] filter = filterString.split(",");
+
+        return Arrays.stream(filter).anyMatch((filter_i) -> {
+
+            if(filter_i.endsWith("*")) {
+                return itemName.startsWith(filter_i.substring(0, filter_i.length() - 1));
+            } else if (filter_i.startsWith("*")) {
+                return itemName.endsWith(filter_i.substring(1));
+            } else {
+                return filter_i.equalsIgnoreCase(itemName);
+            }
+        });
+    }
+
+    @EventHandler
+    public void onInventoryMoveItemEvent(InventoryMoveItemEvent event) {
+
+        if (event.getDestination().getType().equals(InventoryType.HOPPER)
+            && event.getDestination().getHolder() instanceof Container) {
+
+            String customName = ((Container) event.getDestination().getHolder()).getCustomName();
+            if(customName != null) {
+
+                String itemName = event.getItem().getType().getItemTranslationKey();
+                if(!filterMatch(customName, itemName)) {
+                    event.setCancelled(true);
+                }
+            }
+
+        }
+    }
+
+    @EventHandler
+    public void onInventoryPickupItemEvent (InventoryPickupItemEvent event) {
+        if (event.getInventory().getHolder() instanceof Container) {
+            String customName = (((Container) event.getInventory().getHolder()).getCustomName());
+
+            if (customName != null) {
+                String itemName = event.getItem().getItemStack().getType().getItemTranslationKey();
+                if(!filterMatch(customName, itemName)) {
+                    event.setCancelled(true);
+                }
+            }
+        }
+    }
+}
